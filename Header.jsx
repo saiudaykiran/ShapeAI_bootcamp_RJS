ad02e8da25ed42c398c5403b4e9227434b5490b0
saiudaykiran <86521918+saiudaykiran@users.noreply.github.com>
2021-06-26 18:50:56 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+  return(
+    <header>
+      <h1>Book Tickets</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+  return(
+   <div className="note">
+     <h1>Contact</h1>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti eligendi nemo suscipit veniam fuga impedit veritatis neque aperiam, dolorum quasi?</p>
+   </div>
+  );
+}
+
+export default Footer;
